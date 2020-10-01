92f30ea8adf91a2c2bdd052944f1d5777d438d2f
Lipkind <ethan.lipkind@nytimes.com>
2020-09-30 23:04:24 -0400

    render checkboxes in modal

--- index.js
+++ index.js
@@ -12,6 +12,31 @@ const SLACK_URLS = {
 const PAYLOAD_TYPES = {
   BLOCK_ACTIONS: 'block_actions',
   MESSAGE_ACTIONS: 'message_action',
+};
+
+const ACTION_IDS = {
+  MARK_PRIORITY: "mark-important",
+};
+
+const PRIORITY_LEVELS = {
+  LOW: "low",
+  HIGH: "high",
+  CRITICAL: "critical",
+};
+
+const PRIORITY_DESCRIPTIONS = {
+  [PRIORITY_LEVELS.LOW]: {
+    type: 'mrkdwn',
+    text: "This issue is not your top priority - only look into it if there are no higher priority tasks in the backlog."
+  },
+  [PRIORITY_LEVELS.HIGH]: {
+    type: 'mrkdwn',
+    text: "This issue should be handled as soon as possible.",
+  },
+  [PRIORITY_LEVELS.CRITICAL]: {
+    type: 'mrkdwn',
+    text: "This issue requires immediate action."
+  },
 }
 
 // slack POST requests are URL encoded, but the "payload" key is JSON.
@@ -21,36 +46,78 @@ const verifyToken = token => {
   return token === process.env.VERIFICATION_TOKEN;
 };
 
+
+
+const testOptions = Object.keys(PRIORITY_LEVELS).map(key => ({
+  value: key,
+  text: {
+    type: 'plain_text',
+    text: PRIORITY_LEVELS[key],
+  },
+  // description: PRIORITY_DESCRIPTIONS[PRIORITY_LEVELS[key]].text,
+}));
+
 const openModal = (triggerID) => {
   const payload = {
     "trigger_id": triggerID,
     "view": {
       "type": "modal",
-      "callback_id": "modal-identifier",
       "title": {
         "type": "plain_text",
-        "text": "Create an Issue"
+        "text": "Care Bear 🐻❤️🧡💛💚💙💜🖤🐻",
+        "emoji": true
+      },
+      "submit": {
+        "type": "plain_text",
+        "text": "Submit",
+        "emoji": true
+      },
+      "close": {
+        "type": "plain_text",
+        "text": "Cancel",
+        "emoji": true
       },
       "blocks": [
         {
           "type": "section",
-          "block_id": "section-identifier",
           "text": {
-            "type": "mrkdwn",
-            "text": "*Welcome* to ~my~ Block Kit _modal_!"
+            "type": "plain_text",
+            "text": "How would you rank this issue?"
           },
           "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Just a button"
-            },
-            "action_id": "button-identifier"
+            "type": "radio_buttons",
+            "action_id": "this_is_an_action_id",
+            "options": [
+              {
+                "value": PRIORITY_LEVELS.LOW,
+                "text": {
+                  "type": "plain_text",
+                  "text": PRIORITY_LEVELS.LOW
+                }
+              },
+              {
+                "value": PRIORITY_LEVELS.HIGH,
+                "text": {
+                  "type": "plain_text",
+                  "text": PRIORITY_LEVELS.HIGH,
+                }
+              },
+              {
+                "value": PRIORITY_LEVELS.CRITICAL,
+                "text": {
+                  "type": "plain_text",
+                  "text": PRIORITY_LEVELS.CRITICAL,
+                }
+              }
+            ]
           }
-        },
+        }
       ]
     }
   }
+  
+  console.log('sending payload:', JSON.stringify(payload));
+  
   return axios.post(SLACK_URLS.OPEN_VIEWS, payload, {
     headers: {
       Authorization: `Bearer ${process.env.BOT_ACCESS_TOKEN}`
